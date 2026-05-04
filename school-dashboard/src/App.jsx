c2de614e6103aad2fbd7cc7484053fca2fd28bfd
Elvis Gutierrez <144180083+ElvisGutierrez@users.noreply.github.com>
2026-05-04 11:32:08 -0600

    Fix import case for Login component

--- school-dashboard/src/App.jsx
+++ school-dashboard/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Schools from "./pages/Schools";
 import Padres from "./pages/Padres";
